4e892812d2ded63980ba33c3d2836cced4da15a0
sakshidulwani <dulwanisakshi@gmail.com>
2020-09-14 07:24:06 -0700

    Updates

---
+++ precache-manifest.3a0474304eebf49189eb17bf3f9cc0c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb0d6cae69a700f2ed91b30ba382d0b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e63591d7b1966f36bdbc",
+    "url": "/Portfolio/static/css/2.f4da3d2e.chunk.css"
+  },
+  {
+    "revision": "fa40093e93aff1ac5394",
+    "url": "/Portfolio/static/css/main.bd7e4410.chunk.css"
+  },
+  {
+    "revision": "e63591d7b1966f36bdbc",
+    "url": "/Portfolio/static/js/2.b8f405d9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.b8f405d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa40093e93aff1ac5394",
+    "url": "/Portfolio/static/js/main.f977afbe.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
