5db7b1c35c8e3c9b728c450cb0b04926cf167c0e
sakshidulwani <dulwanisakshi@gmail.com>
2020-09-14 07:35:54 -0700

    initial commit

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -19,53 +19,41 @@ function Skills(props) {
                   <p className="separator " />
                   </div>
 
-                  <Row className="progress_row mt-5">
+                  <Row className="progress_row mt-5 text-center">
                   
                   <Col xs={6} sm={4} md={4} lg={2} className="mx-auto my-4">
                   <div>
-                  <CircularProgressbarWithChildren value={70} strokeWidth={5} className="progress_bar" styles={buildStyles({pathColor: "#65E8FF"})}>
-                    <img style={{ width: 60, marginTop: -5 }} src="https://www.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt=""/>
-                  </CircularProgressbarWithChildren>
+                    <img height="70px" src="https://www.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt=""/>
                   </div>
                   </Col>
 
                   <Col xs={6} sm={4} md={4} lg={2} className=" mx-auto my-4">
                   <div>
-                  <CircularProgressbarWithChildren value={70} strokeWidth={5} className="progress_bar" styles={buildStyles({pathColor: "#FFD600"})}>
-                    <img style={{ width: 60, marginTop: -5 }} src="https://img.icons8.com/color/000000/javascript.png" alt=""/>
-                  </CircularProgressbarWithChildren>
+                    <img height="70px" src="https://img.icons8.com/color/000000/javascript.png" alt=""/>
                   </div>
                   </Col>
 
                   <Col xs={6} sm={4} md={4} lg={2} className=" mx-auto my-4">
                   <div>
-                  <CircularProgressbarWithChildren value={60} strokeWidth={5} className="progress_bar" styles={buildStyles({pathColor: "#29B6F6"})}>
-                    <img style={{ width: 60, marginTop: -5 }} src="https://img.icons8.com/color/000000/material-ui.png" alt=""/>
-                  </CircularProgressbarWithChildren>
+                    <img height="70px" src="https://img.icons8.com/color/000000/material-ui.png" alt=""/>
                   </div>
                   </Col>
 
                   <Col xs={6} sm={4} md={4} lg={2} className=" mx-auto my-4">
                   <div>
-                  <CircularProgressbarWithChildren value={90} strokeWidth={5} className="progress_bar" styles={buildStyles({pathColor: "#673AB7"})}>
-                    <img style={{ width: 60, marginTop: -5 }} src="https://img.icons8.com/color/000000/bootstrap.png" alt=""/>
-                  </CircularProgressbarWithChildren>
+                    <img height="70px" src="https://img.icons8.com/color/000000/bootstrap.png" alt=""/>
                   </div>
                   </Col>
 
                   <Col xs={6} sm={4} md={4} lg={2} className=" mx-auto my-4">
                   <div>
-                  <CircularProgressbarWithChildren value={90} strokeWidth={5} className="progress_bar" styles={buildStyles({pathColor: "#FF6D00"})}>
-                    <img style={{ width: 60, marginTop: -5 }} src="https://img.icons8.com/color/000000/html-5.png" alt="" />
-                  </CircularProgressbarWithChildren>
+                    <img height="70px" src="https://img.icons8.com/color/000000/html-5.png" alt="" />
                   </div>
                   </Col>
 
                   <Col xs={6} sm={4} md={4} lg={2} className=" mx-auto my-4">
                   <div>
-                  <CircularProgressbarWithChildren value={65} strokeWidth={5} className="progress_bar" styles={buildStyles({pathColor: "#0277BD"})}>
-                    <img style={{ width: 60, marginTop: -5 }} src="https://img.icons8.com/color/000000/css3.png" alt=""/>
-                  </CircularProgressbarWithChildren>
+                    <img height="70px" src="https://img.icons8.com/color/000000/css3.png" alt=""/>
                   </div>
                   </Col>
                   </Row>
